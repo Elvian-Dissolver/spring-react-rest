1a50c6c51b417aaef3c1e704c44656c226a1ba35
DissoLver <indra.elvian@gmail.com>
2018-10-02 14:50:19 +0700

    Add parent and user class

--- src/main/java/models/UserRepository.java
+++ src/main/java/models/UserRepository.java
@@ -1,4 +1,11 @@
 package models;
 
-public interface UserRepository {
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CassandraRepository<User, Integer> {
+
+
+    @Query("SELECT *FROM user WHERE age<=?0 ALLOW FILTERING")
+    List<User> findByAgeLessThanEqual(int age);
 }
